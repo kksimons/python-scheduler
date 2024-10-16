b40571a42b65632822d2b550a3b30ca2d3526730
kksimons <kksimons@gmail.com>
2024-10-16 14:02:18 -0600

    allow for cors

--- app/app.py
+++ app/app.py
@@ -5,7 +5,17 @@ import uvicorn
 
 app = FastAPI()
 
-# python -m uvicorn main:app --reload   
+# For CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite runs on 5173
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow everything
+    allow_headers=["*"],
+)
+
+
+# python -m uvicorn main:app --reload
 # Define the Pydantic model to accept the input JSON
 class ShiftScheduleInput(BaseModel):
     num_employees: int
@@ -13,28 +23,28 @@ class ShiftScheduleInput(BaseModel):
     total_days: int
     employee_types: list[str]  # List of employee types ('full_time' or 'part_time')
 
+
 # Root endpoint
 @app.get("/")
 def read_root():
     return {"message": "Employee Shift Scheduler is up!"}
+
 
 # Scheduler endpoint
 @app.post("/api/v1/scheduler")
 def scheduler(data: ShiftScheduleInput):
     # Create the model based on the input data
     model, shifts = create_shift_scheduling_model(
-        data.num_employees,
-        data.shifts_per_day,
-        data.total_days,
-        data.employee_types
+        data.num_employees, data.shifts_per_day, data.total_days, data.employee_types
     )
 
     # Solve the model and return the results
     results = solve_shift_scheduling(
         model, shifts, data.num_employees, data.shifts_per_day, data.total_days
     )
-    
+
     return {"schedules": results}
+
 
 if __name__ == "__main__":
     uvicorn.run(app="app:app", host="0.0.0.0", port=8502, reload=True)
