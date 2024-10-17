8604e4229890cd554c56399822166eba1c444518
Kyle Simons <108242292+kksimons@users.noreply.github.com>
2024-10-16 19:04:41 -0600

    more cors config

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # For CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite runs on 5173
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allow everything
     allow_headers=["*"],
